457459f6658a72490e03aae80baa667e52e0751c
MrNiceWangwendi <897136043@qq.com>
2017-11-11 00:27:44 +0800

    接入dubbo

--- blogforum-sso-web/src/main/java/com/blogforum/sso/web/conf/DubboConfig.java
+++ blogforum-sso-web/src/main/java/com/blogforum/sso/web/conf/DubboConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.PropertySource;
  *
  */
 @Configuration
-@PropertySource("classpath:dubbo/dubbo.properties")
 @ImportResource({"classpath:dubbo/*.xml"})
 public class DubboConfig {
 
