d60ef78cd08c5a21842c452d7b576090fb1fed83
MrNiceWangwendi <897136043@qq.com>
2017-12-05 23:45:43 +0800

    修改配置

--- blogforum-sso-facade/src/main/java/com/blogforum/sso/facade/model/BaseSO.java
+++ blogforum-sso-facade/src/main/java/com/blogforum/sso/facade/model/BaseSO.java
@@ -2,6 +2,9 @@ package com.blogforum.sso.facade.model;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class BaseSO implements Serializable {
 
 	/**
@@ -9,4 +12,8 @@ public class BaseSO implements Serializable {
 	 */
 	private static final long serialVersionUID = -5360049315527145536L;
 
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
